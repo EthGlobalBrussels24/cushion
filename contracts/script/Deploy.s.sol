89078d9a55ae5ff06ee8bf10f9bf941d51053185
Alexe Spataru <alexeluca.spataru@gmail.com>
2024-07-14 03:18:46 +0200

    hook deployment

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,26 +3,37 @@ pragma solidity >=0.8.25 <0.9.0;
 
 import { BaseScript } from "./Base.s.sol";
 import { IPoolManager } from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import { Hooks } from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import { VaultFactory } from "src/VaultFactory.sol";
-import { MockERC20 } from "src/MockERC20.sol";
 import { WrappedNative } from "src/WrappedNative.sol";
 import { CushionHook } from "src/CushionHook.sol";
 import { console2 } from "forge-std/src/console2.sol";
 
+import { HookMiner } from "./HookMiner.sol";
+
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
   IPoolManager constant BASE_SEPOLIA_POOL_MANAGER = IPoolManager(0xFC885F37F5A9FA8159c8dBb907fc1b0C2fB31323);
+  address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 
   function run() public broadcast {
     address wrappedNative = address(new WrappedNative());
     VaultFactory factory = new VaultFactory(broadcaster, BASE_SEPOLIA_POOL_MANAGER, wrappedNative);
-    // TODO: deploy hook at specific address
-    CushionHook hook = new CushionHook(BASE_SEPOLIA_POOL_MANAGER, factory);
+
+    uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_REMOVE_LIQUIDITY_FLAG);
+    // Mine a salt that will produce a hook address with the correct flags
+    (address hookAddress, bytes32 salt) = HookMiner.find(
+      CREATE2_DEPLOYER,
+      flags,
+      type(CushionHook).creationCode,
+      abi.encode(address(BASE_SEPOLIA_POOL_MANAGER), address(factory))
+    );
+
+    CushionHook hook = new CushionHook{ salt: salt }(BASE_SEPOLIA_POOL_MANAGER, factory);
     factory.setHook(address(hook));
 
-    MockERC20 token0 = new MockERC20("Mock Token", "MOCK", 1e18);
-    MockERC20 token1 = new MockERC20("Mock Token", "MOCK", 1e18);
+    factory.launchCushionToken("Mock Token", "MOCK", 1e18);
 
-    factory.createInsuredPool(address(token0), address(token1));
+    console2.log("Deployed CushionHook at", address(hook));
   }
 }
--- contracts/src/vaultFactory.sol
+++ contracts/src/vaultFactory.sol
@@ -34,15 +34,10 @@ contract VaultFactory is Ownable {
     hook = newHook;
   }
 
-  function createInsuredPool(address token0, address token1) external {
-    _initializeUniPool(token0, token1, address(0), 79228162514264337593543950336, bytes(""));
-  }
-
   function launchCushionToken(
     string memory name,
     string memory symbol,
-    uint256 totalSupply,
-    uint160 initialPrice
+    uint256 totalSupply
   ) external returns (address token, address vault) {
     token = address(new MockERC20(name, symbol, totalSupply));
     vault = address(new Vault(MockERC20(token)));
@@ -51,7 +46,7 @@ contract VaultFactory is Ownable {
     vaults.push(vault);
     tokenToVault[token] = vault;
 
-    _initializeUniPool(token, wrappedNative, hook, initialPrice, abi.encode(vault));
+    _initializeUniPool(token, wrappedNative, hook, 79228162514264337593543950336, abi.encode(vault));
   }
 
   function increaseCushionRiskFactor(address vault, uint256 amountInsured, uint256 amountWNative) external {
